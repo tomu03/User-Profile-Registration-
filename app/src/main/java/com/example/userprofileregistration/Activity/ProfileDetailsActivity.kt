0b7e4f02151bb40c094c222019cc72cee0fdfa60
tomu03 <369tomu@gamil.com>
2024-05-14 13:24:50 +0600

    Backend Activity are add

--- app/src/main/java/com/example/userprofileregistration/Activity/ProfileDetailsActivity.kt
+++ app/src/main/java/com/example/userprofileregistration/Activity/ProfileDetailsActivity.kt
@@ -1,13 +1,41 @@
 package com.example.userprofileregistration.Activity
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.userprofileregistration.Database.UserProfile
 import com.example.userprofileregistration.R
 
 class ProfileDetailsActivity : AppCompatActivity() {
+
+    private lateinit var userProfile: UserProfile
+
+    private lateinit var nameTextView: TextView
+    private lateinit var emailTextView: TextView
+    private lateinit var dobTextView: TextView
+    private lateinit var districtTextView: TextView
+    private lateinit var mobileTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_details)
+
+        userProfile = intent.getSerializableExtra("USER_PROFILE") as UserProfile
+
+        nameTextView = findViewById(R.id.nameTextView)
+        emailTextView = findViewById(R.id.emailTextView)
+        dobTextView = findViewById(R.id.dobTextView)
+        districtTextView = findViewById(R.id.districtTextView)
+        mobileTextView = findViewById(R.id.mobileTextView)
+
+        populateFields()
+    }
+
+    private fun populateFields() {
+            nameTextView.text = userProfile.name
+            emailTextView.text = userProfile.email
+            dobTextView.text = userProfile.dob
+            districtTextView.text = userProfile.district
+            mobileTextView.text = userProfile.mobile
 
     }
 }
--- app/src/main/java/com/example/userprofileregistration/Activity/ProfileListActivity.kt
+++ app/src/main/java/com/example/userprofileregistration/Activity/ProfileListActivity.kt
@@ -1,13 +1,57 @@
 package com.example.userprofileregistration.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.userprofileregistration.Adapter.ProfileAdapter
 import com.example.userprofileregistration.R
+import com.example.userprofileregistration.ViewModel.UserProfileViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ProfileListActivity : AppCompatActivity() {
+
+    private lateinit var profileViewModel: UserProfileViewModel
+    private lateinit var profileAdapter: ProfileAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_list)
 
+        profileViewModel = ViewModelProvider(this).get(UserProfileViewModel::class.java)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.profileRecyclerView)
+        profileAdapter = ProfileAdapter()
+
+        recyclerView.adapter = profileAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        profileViewModel.getUserProfiles().observe(this, Observer { profiles ->
+            profileAdapter.submitList(profiles)
+        })
+
+        profileAdapter.setOnItemClickListener { userProfile ->
+            val intent = Intent(this@ProfileListActivity, ProfileDetailsActivity::class.java)
+            intent.putExtra("USER_PROFILE", userProfile)
+            startActivity(intent)
+        }
+
+            profileAdapter.setOnDeleteClickListener { userProfile ->
+                profileViewModel.deleteUserProfile(userProfile)
+            }
+
+
+            profileAdapter.setOnUpdateClickListener { userProfile ->
+                val intent = Intent(this@ProfileListActivity, ProfileUpdateActivity::class.java)
+                intent.putExtra("USER_PROFILE", userProfile)
+                startActivity(intent)
+            }
+
+            findViewById<FloatingActionButton>(R.id.addProfileBtn).setOnClickListener {
+                val intent = Intent(this@ProfileListActivity, ProfileAddActivity::class.java)
+                startActivity(intent)
+            }
     }
 }
